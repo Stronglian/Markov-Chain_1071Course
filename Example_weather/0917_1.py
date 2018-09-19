87b5b3e7df5b55772be4ef972276744d7aae841e
Stronglian <strong.85.wu@gmail.com>
2018-09-19 08:18:23 +0800

    ver. 1 Done
    不確定題目預測目標可以再再改
    - 註解
    - 頻率浮動是否有點高

--- Example_weather/0917_1.py
+++ Example_weather/0917_1.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Sep 17 16:57:03 2018
-
-@author: StrongPria
 """
 """
 assert:
@@ -18,16 +16,19 @@ class MarkovChain_weather():
         #Rainy(R), Cloudy(C), Sunny(S)
         self.tableW = { 'R':0, 'C':1, 'S':2}
         self.probabilityMatrix = np.array([[0.4, 0.3, 0.3],
-                                          [0.2, 0.6, 0.2],
-                                          [0.1, 0.1, 0.8]])
-        self.frequency = 10000
+                                           [0.2, 0.6, 0.2],
+                                           [0.1, 0.1, 0.8]])
+        self.frequency = 100000
         self.randomNum = 1000
-        self.days = 3
-        self.firstDay = 'S'
-    def NextWeather(self, today):
-        assert today in self.tableW.keys()
-        indexW = self.tableW[today]
+        
+        self.firstDayW = 'S'
+#        self.days = 3
+    def NextWeather(self, todayW):
+        #依照機率表(probabilityMatrix)與當天天氣(todayW)與猜測隔天天氣。
+        assert todayW in self.tableW.keys()
+        indexW = self.tableW[todayW]
         probW = self.probabilityMatrix[indexW]
+        
         tempRan = random.randrange(0, self.randomNum)
         if tempRan < self.randomNum*probW[0]:
             return 'R'
@@ -35,18 +36,37 @@ class MarkovChain_weather():
             return 'C'
         else:
             return 'S'
-    def CalOneRound(self):
+    def CalOneRound(self, days=3):#, firstDayW='S'):
+        #計算單次連續天數(days)的天氣可能為何(weaherSeq)。
         weaherSeq = ''
-        todaydayW = self.firstDay
-        for d in range(self.days):
+        todaydayW = self.firstDayW
+#        todaydayW = firstDayW
+        for d in range(days):
             todaydayW = self.NextWeather(todaydayW)
             weaherSeq += todaydayW
         return weaherSeq
-
-
+    def Predict(self, firstDayW='S', targetSeq = 'SSSRRSCS'):
+        #計算指定天數(len(targetSeq))下，當今天天氣為 firstDayW，往後天氣會依序發生(targetSeq)的機率。
+        assert firstDayW in self.tableW.keys()
+        self.firstDayW = firstDayW
+        days = len(targetSeq)
+        targetNum = 0
+        for d in range(self.frequency):
+            preW = self.CalOneRound(days = days)
+            if preW == targetSeq:
+                targetNum +=1
+        print('The', targetSeq,'appeared',targetNum,'times in',self.frequency,'times.')
+    
+        return round(targetNum*100 / self.frequency,2)
 
 if __name__ == '__main__' :
+    import time
+    startTime = time.time()
     print("START")
     test = MarkovChain_weather()
-#    print (test.NextWeather('R'))
-    print(test.CalOneRound())
+#   
+    print (test.NextWeather('R'))
+    print(test.Predict(firstDayW='S', targetSeq = 'SRC'),'%')
+    
+    endTime = time.time()
+    print('END', 'It takes', endTime-startTime ,'sec.')
