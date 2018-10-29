6364d0a2cbe40ae3281d8ec605b0ef1d6e49df29
Stronglian <strong.85.wu@gmail.com>
2018-10-29 18:20:23 +0800

    ToBeClass
    為了要能被call所以更名

--- Example3_Dice/c1015_3_5_0_ViterbiAlgorithm.py
+++ Example3_Dice/c1015_3_5_0_ViterbiAlgorithm.py
@@ -40,50 +40,60 @@ class HidenMarkovModel_Viterbi():
         
     def CalRoPsiTable(self, target):
         """ """
-        T = len(target)
+        lenTarget = len(target)
         # step 0 - table #table - time(-1) - state #ρ (ro)、ψ(psi)
         ro  = np.zeros((len(target), self.stateNumber), dtype = np.float32)
         psi = np.ones((len(target), self.stateNumber), dtype = np.float32) * -1
         # step 1 – Initialization #psi DONE before
-        ro[0,:]  = self.initialStateProb * self.probabilityMatrix[:, self.stateOutput == target[0]].T
+        ro[0,:]  = self.initialStateProb * self.probabilityMatrix[:, self.stateOutput == target[0]].T[0]
+#        print(self.initialStateProb ,"\n",  self.probabilityMatrix[:, self.stateOutput == target[0]].T[0], "\n")
 #        psi[0,:] = np.array([ -1 for i in range(self.stateNumber)])#0 重複意義，故換-1
         # step 2 – Recursion
+        tmpArr = np.zeros((2,2))
         for t in range(1, len(target)):
-            for s in range(self.stateNumber):
-                ro[t, s]  = (ro[t-1, s] * self.stateChangeMatrix[:, s]).max() *     \
-                            self.probabilityMatrix[s, self.stateOutput == target[t]]
-                psi[t, s] = (ro[t-1, s] * self.stateChangeMatrix[:, s]).argmax() #*  \
-#                            self.probabilityMatrix[s, self.stateOutput == target[t]] #不用算入，因為乘了相對大小還是不變
+#            for s in range(self.stateNumber):
+#                tmpArr[:, s] = ro[t-1, s] * self.stateChangeMatrix[:, s] * self.probabilityMatrix[s, self.stateOutput == target[t]]
+##                print((ro[t-1, s] * self.stateChangeMatrix[:, s]).max())# *     \
+##                ro[t, s]  = (ro[t-1, s] * self.stateChangeMatrix[:, s]).max() *     \
+##                            self.probabilityMatrix[s, self.stateOutput == target[t]]
+##                psi[t, s] = (ro[t-1, s] * self.stateChangeMatrix[:, s]).argmax() #*  \
+###                            self.probabilityMatrix[s, self.stateOutput == target[t]] #不用算入，因為乘了相對大小還是不變
+            tmpArr[:, :] = ro[t-1, :] * self.stateChangeMatrix[:, :] * self.probabilityMatrix[:, self.stateOutput == target[t]].T[0]
+            ro[t, :]  = (tmpArr).max(axis = 1)
+            psi[t, :] = (tmpArr).argmax(axis = 1) 
+            
+        self.roTable = ro.copy()
+        self.psiTable = psi.copy()
         # step 3 – Termination
-        P_star_all = ro[-1,:].max()    #P*
-        iT_all     = ro[-1,:].argmax() #i*
+        P_star_all = ro[-1,:].max().copy()    #P*
+        iT_all     = ro[-1,:].argmax().copy() #i*
 #        print(P_all, iT_all)
         # step 4 – Path (state sequence) backtracking
-        stateSeqIndex = np.array([-1 for i in range(T)])
+        stateSeqIndex = np.array([-1 for i in range(lenTarget)])
 #        print(stateSeqIndex)
         stateSeqIndex[-1] = iT_all
         for t in range(len(target)-1, 0, -1):
 #        for t, index in list(enumerate(reversed(iT_all)))[1:]:
 #            print(stateSeqIndex)
             stateSeqIndex[t-1] = psi[t, stateSeqIndex[t]]
-        print(stateSeqIndex)
+#        print(stateSeqIndex)
         #另外轉存
-        self.roTable = ro
-        self.psiTable = psi
+#        self.roTable = ro.copy()
+#        self.psiTable = psi.copy()
         return ro, psi, P_star_all, stateSeqIndex
     
     def Predict_optimalStateSequence_useRoPsi(self,target, boolPrint = True):
         """ """
         ro, psi, bestProb, bestStateSquenceIndex = self.CalRoPsiTable(target)
         #The best state sequence having the highest probability
-#        if boolPrint:
-        print('"',target,'"',"'s Probability:", bestProb) #,beta[0,:].sum(dtype = np.float32))
+        if boolPrint:
+            print('"',target,'"',"'s Probability:", bestProb) #,beta[0,:].sum(dtype = np.float32))
         seqLis = []
         for indexTmp in bestStateSquenceIndex: #ro[:,:].argmax(axis = 1):
             seqLis.append(self.stateName[indexTmp])
         if boolPrint:
             print('"',target,'"',"'s Optimal State Sequence is", seqLis)
-        return bestProb
+        return bestProb, bestStateSquenceIndex
     
 if __name__ == '__main__' :
     import time
@@ -91,13 +101,15 @@ if __name__ == '__main__' :
     print("START\n\n")
     
     test = HidenMarkovModel_Viterbi()
+    
 #    test.Predict_optimalStateSequence_useRoPsi(target = "666")
 #    test.Predict_optimalStateSequence_useRoPsi(target = "123456")
-    test.Predict_optimalStateSequence_useRoPsi(target = "664321")# state seq!
+#    test.Predict_optimalStateSequence_useRoPsi(target = "664321")# state seq!
 #    test.Predict_optimalStateSequence_useRoPsi(target = "111666")
 #    test.Predict_optimalStateSequence_useRoPsi(target = "162636")
 #    test.Predict_optimalStateSequence_useRoPsi(target = "126656")
-#    ro, psi = test.roTable, test.psiTable
+    test.Predict_optimalStateSequence_useRoPsi(target = "1"*10+"6"*6)
+    ro, psi = test.roTable, test.psiTable
     
 #    aLL = 0
 #    for s_0 in range(1,7):
---
+++ Example3_Dice/c1029_3_1_1_genaralForAccuracyCal.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  8 18:38:23 2018
+
+@title: unfair dice
+"""
+
+
+import numpy as np
+import random
+
+class HidenMarkovModel_genaral():
+    def __init__(self):
+        self.frequency = 1000000
+        self.randomNum = 10000
+        self.initialStateProb   = np.array([0.6, 0.4]) #[fair, unfair]
+        # 0, 1, 2 #[i][j] 在state i 時，換到state j的機率
+        self.stateChangeMatrix = np.array([[0.95, 0.05],
+                                           [0.10, 0.90]]) 
+        #[i][j] 在state i 時，生成j的機率    
+        self.probabilityMatrix = np.array([[ 1/6,  1/6,  1/6,  1/6,  1/6,  1/6], #fair 
+                                           [1/10, 1/10, 1/10, 1/10, 1/10,  1/2]])#unfair 
+        self.stateOutput = [str(i) for i in range(1, len(self.probabilityMatrix[0,:])+1)]
+        return
+    def CalOutput(self, currectState = None, probM = None, output=[]):
+        """ 算機率的共用函數"""
+#        assert len(probM) == len(output) #確認輸入正確
+        if not len(probM) == len(output):
+            print(currectState, probM, output)
+            raise AssertionError
+#        tempRan = random.randrange(0, self.randomNum)
+        tempRan = np.random.randint(self.randomNum)
+        for i in range(len(output)):
+            if tempRan < self.randomNum*probM[:i+1].sum(): #總和到i項
+                return output[i]
+                break #多的
+    def NextState(self, currectState = None, probState = None):
+        """依照機率算下一個狀態(使用的骰子)"""
+        #分辨 initial
+        if currectState != None:#probState == None:# and 
+            probState = self.stateChangeMatrix[currectState]
+        
+        return self.CalOutput(currectState = currectState, 
+                         probM = probState, 
+                         output=[i for i in range(len(probState))]) #可共用
+    def OutputAtState(self, currectState):
+        """依照機率與現有狀態得出現機率"""
+        return self.CalOutput(currectState = currectState, 
+                         probM = self.probabilityMatrix[currectState], 
+                         output= self.stateOutput)
+    def CalOneRound(self, repeatTimes=5):
+        """執行一次的內容；並且記錄狀態於 stateSeq"""
+        outputSeq = ''
+        stateSeq = []
+        #初始
+        currentState = self.NextState(probState = self.initialStateProb)
+        outputSeq += self.OutputAtState(currentState)
+        stateSeq.append(currentState)
+        #重複
+        for t in range(1, repeatTimes):#因為第一次已經跑了
+            currentState = self.NextState(currectState = currentState)
+            outputSeq += self.OutputAtState(currentState)
+            stateSeq.append(currentState)
+        return outputSeq, stateSeq
+    def Predict(self, targetSeq, printTF = True):
+        """計算為目標序列(targetSeq)的機率"""
+        repeatTimes = len(targetSeq)
+        targetNum = 0
+        for t in range(self.frequency):
+            preO = self.CalOneRound(repeatTimes = repeatTimes)
+            if preO == targetSeq:
+                targetNum +=1
+        if printTF:
+            print('The', targetSeq,'appeared',targetNum,'times in',self.frequency,'times.')
+    
+        return (float(targetNum)/ self.frequency)
+if __name__ == '__main__' :
+    import time
+    startTime = time.time()
+    print("START")
+    test = HidenMarkovModel_genaral()
+    
+    print("Probability is", test.Predict(targetSeq = '123456'))
+
+    endTime = time.time()
+    print('\n\n\nEND,', 'It takes', endTime-startTime ,'sec.')
