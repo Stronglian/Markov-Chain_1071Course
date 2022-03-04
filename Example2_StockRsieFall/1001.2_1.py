03124be6a9b6b7f622a7d377f030a3708a77b6d9
Strong Wu <22480536+Stronglian@users.noreply.github.com>
2022-03-04 14:52:50 +0800

    Update new Quest

--- Example2_StockRsieFall/1001.2_1.py
+++ Example2_StockRsieFall/1001.2_1.py
@@ -8,7 +8,7 @@ Created on Mon Oct  1 14:48:13 2018
 """
 """
 先做了兩項相似函數的整合，但仍保留原有的名稱
-作業二 ，達成 targetSeq 的最佳路徑(state)為何?(未進行)
+作業二 ，達成 targetSeq 的最佳路徑(state)為何?
 紀錄array：路徑、機率、結果
 找符合結果的最大機率，輸出路徑
 """
@@ -29,6 +29,8 @@ class HidenMarkovModel():
                                            [0.1, 0.6, 0.3],
                                            [0.3, 0.3, 0.4]])
         self.initialStateProb = np.array([0.5, 0.2, 0.3])
+        # 達成 targetSeq 的最佳路徑(state)為何?
+        self.dictHisState = dict()
         return
 
     def CalOutput(self, currectState: np.int, probM: np.ndarray, output: list):
@@ -68,43 +70,68 @@ class HidenMarkovModel():
     def CalOneRound(self, repeatTimes: np.int = 5):
         """"""
         outputSeq = ""
+        arrHisState = np.ones(repeatTimes) * -1
         #初始
         currentState = self.NextState(probState=self.initialStateProb)
-        outputSeq += self.OutputAtState(currentState)
         #重複
-        for t in range(repeatTimes - 1):  #-1因為第一次已經跑了
-            currentState = self.NextState(currectState=currentState)
+        for t in range(repeatTimes):
+            # 上一輪收尾
             outputSeq += self.OutputAtState(currentState)
-        return outputSeq
+            arrHisState[t] = currentState
+            # 進入下一輪
+            currentState = self.NextState(currectState=currentState)
+        return outputSeq, arrHisState
 
-    def Predict(self, targetSeq: str, printTF: bool = True):
+    def Predict(self, targetSeq: str, printTF: bool = True, intRank:int = 3):
         """計算為目標序列(targetSeq)的機率"""
         repeatTimes = len(targetSeq)
         targetNum = 0
+
+
         for t in range(self.frequency):
-            preO = self.CalOneRound(repeatTimes=repeatTimes)
+            preO, hisState = self.CalOneRound(repeatTimes=repeatTimes)
             if preO == targetSeq:
                 targetNum += 1
+            self.RecordHistoryOfState(targetSeq, hisState)
         if printTF:
             print("The", targetSeq, "appeared", targetNum, "times in",
                   self.frequency, "times.")
-
+        if intRank:
+            # max(self.dictHisState[targetSeq], key=self.dictHisState[targetSeq].get)
+            rankHis = sorted(self.dictHisState[targetSeq],
+                             key=self.dictHisState[targetSeq].get,
+                             reverse=True)
+            print(f" - optm state road: {rankHis[:intRank]}")
         return (float(targetNum) / self.frequency)
 
     def CalMaxMin(self, targetSeq, times=10, printTF=False):
         """ 跑 times 次，找最大最小機率 """
         minP, maxP, avgP = 1, 0, 0
         for i in range(times):
-            num = test.Predict(targetSeq=targetSeq, printTF=printTF)
+            # 初始化
+            self.dictHisState[targetSeq] = dict()
+            num = self.Predict(targetSeq=targetSeq, printTF=printTF)
+            # 輸出最多
+            
+            # 計算極值
             avgP += num
             if num > maxP:
                 maxP = num
             if num < minP:
                 minP = num
         avgP /= times
-        print(f"run {times} times, max: {maxP}, min: {minP} avg: {avgP}")
+        print(f"run {times} times, max: {maxP}, min: {minP} avg: {avgP:6}")
         return
 
+    def RecordHistoryOfState(self, targetSeq:str, hisState:(np.ndarray, list)):
+        if targetSeq not in self.dictHisState.keys():
+            self.dictHisState[targetSeq] = dict()
+        strHisState = "".join(hisState.astype(np.int).astype(str))
+        if strHisState not in self.dictHisState[targetSeq].keys():
+            self.dictHisState[targetSeq][strHisState] = 1
+        else:
+            self.dictHisState[targetSeq][strHisState] += 1
+        return
 
 if __name__ == "__main__":
     import time
@@ -112,8 +139,8 @@ if __name__ == "__main__":
     print("START")
     test = HidenMarkovModel()
 
-    #    print("Probability is", test.Predict(targetSeq="↑↑↑↑↑"))
+    # print("Probability is", test.Predict(targetSeq="↑↑↑↑↑"))
     test.CalMaxMin(targetSeq="↑↑↑↑↑", printTF=True)
 
     endTime = time.time()
-    print("\n\n\nEND, It takes {endTime - startTime} sec.")
+    print(f"\n\n\nEND, It takes {endTime - startTime} sec.")
